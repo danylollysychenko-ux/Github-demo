439cd13f3a568af53d0e57b7c697d818f7abc5a4
danylollysychenko-ux <danylollysychenko@gmail.com>
2026-01-12 08:38:58 -0500

    Fix print statement syntax in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("I like cookies")
 print("Hello I'm here for git practice - Remote")
 print("Sometimes I think about how cats are better than cats")
-print("Sometimes I realize that Mr.Klins does what he is supposed to do.)
+print("Sometimes I realize that Mr.Klins does what he is supposed to do.")
